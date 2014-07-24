c98c654a33dcfc4d63b76f7518850435f4fe70a7
samynk <samyn.koen@gmail.com>
2014-07-24 13:50:52 +0200

    Removed unused import.

--- src/dae/prefabs/ui/ParameterPanel.java
+++ src/dae/prefabs/ui/ParameterPanel.java
@@ -12,7 +12,6 @@ import java.awt.GridBagConstraints;
 import java.util.HashMap;
 import javax.swing.JComponent;
 import javax.swing.border.TitledBorder;
-import sun.misc.JavaxSecurityAuthKerberosAccess;
 
 /**
  *
