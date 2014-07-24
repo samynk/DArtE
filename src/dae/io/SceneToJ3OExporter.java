b567014a6935d6e483f457b9d64d362e98ad4da2
samynk <samyn.koen@gmail.com>
2014-07-24 13:26:12 +0200

    Exports a simplified version of the scene, specifically for the JMonkeyEngine sdk.

--- src/dae/io/SceneToJ3OExporter.java
+++ src/dae/io/SceneToJ3OExporter.java
@@ -6,17 +6,21 @@
 package dae.io;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.export.binary.BinaryExporter;
 import com.jme3.scene.Node;
+import dae.io.export.Exporter;
 import dae.io.game.GameSceneLoader;
 import dae.project.Level;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  * This class exports a given scene to a J3O file. All the specific
  * sandbox constructs ( waypoints, cameras, ... ) are exported as dummies.
  * @author Koen Samyn
  */
-public class SceneToJ3OExporter {
+public class SceneToJ3OExporter implements Exporter{
 
     /**
      * Write the scene to a file.
@@ -24,11 +28,14 @@ public class SceneToJ3OExporter {
      * @param location The location of the file.
      * @param node the root node of the scene.
      */
-    public static void writeScene(String location, AssetManager manager, Level level) {
-        File levelLocation = level.getLocation();
+    public void writeScene(File location, AssetManager manager, Level level) {
+        File levelLocation = level.getAbsoluteLocation();
         Node result = new Node(level.getName());
         GameSceneLoader.loadScene(levelLocation, manager, result);
-        
-        
+        try {
+            BinaryExporter.getInstance().save(result, location);
+        } catch (IOException ex) {
+            Logger.getLogger(SceneToJ3OExporter.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
     }
 }
