33a55069d476f7fe3c1e9489d490cf80c29ff0fb
samynk <samyn.koen@gmail.com>
2014-07-25 08:21:24 +0200

    Added support for PropertyConverter.

--- src/dae/prefabs/parameters/Parameter.java
+++ src/dae/prefabs/parameters/Parameter.java
@@ -4,6 +4,10 @@
  */
 package dae.prefabs.parameters;
 
+import dae.prefabs.parameters.converter.PropertyConverter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Koen
@@ -12,8 +16,8 @@ public class Parameter {
 
     private String type;
     private String id;
-    
     private String label;
+    private PropertyConverter converter;
 
     public Parameter(String type, String id) {
         this.type = type;
@@ -54,5 +58,36 @@ public class Parameter {
      */
     public void setLabel(String label) {
         this.label = label;
+    }
+
+    /**
+     * Sets the converter class to use for this parameter.
+     *
+     * @param converter the converter class to use.
+     */
+    public void setConverter(String converterClass) {
+        if (converterClass == null || converterClass.length()==0)
+            return;
+        Class clazz;
+        try {
+            clazz = Class.forName(converterClass);
+            if (PropertyConverter.class.isAssignableFrom(clazz)) {
+                converter = (PropertyConverter) clazz.newInstance();
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Parameter.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Parameter.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Parameter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Returns the property converter for this parameter.
+     * @return the property converter object.
+     */
+    public PropertyConverter getConverter(){
+        return converter;
     }
 }
