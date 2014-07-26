5a62b1362a358d1903d80398623c36965952c961
samynk <samyn.koen@gmail.com>
2014-07-26 11:42:35 +0200

    Removed unnecessary imports.

--- src/dae/prefabs/shapes/ArrowShape.java
+++ src/dae/prefabs/shapes/ArrowShape.java
@@ -4,13 +4,9 @@
  */
 package dae.prefabs.shapes;
 
-import com.jme3.asset.AssetManager;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
-import com.jme3.scene.Geometry;
 import com.jme3.scene.Mesh;
 import com.jme3.scene.VertexBuffer;
 import com.jme3.util.BufferUtils;
