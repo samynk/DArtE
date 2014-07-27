fbb1665cfabbb4c0c1bd0256e417031f18601b40
samynk <samyn.koen@gmail.com>
2014-07-27 23:33:49 +0200

    A prefab can now have a FuzzySystem parameter.

---
+++ src/dae/prefabs/parameters/FuzzyParameter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dae.prefabs.parameters;
+
+/**
+ *
+ * @author Koen Samyn
+ */
+public class FuzzyParameter extends Parameter{
+
+    /**
+     * Creates a new Fuzzy Parameter UI.
+     * @param id 
+     */
+    public FuzzyParameter(String type,String id){
+        super(type,id);
+    }
+}
