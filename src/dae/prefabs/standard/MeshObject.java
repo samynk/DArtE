391632b3899c236ceb028a2ded45e865c4f9e180
samynk <samyn.koen@gmail.com>
2014-07-27 23:35:08 +0200

    MeshObject can now be used inside a rig (BodyElement implementation).

--- src/dae/prefabs/standard/MeshObject.java
+++ src/dae/prefabs/standard/MeshObject.java
@@ -13,14 +13,19 @@ import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 import dae.GlobalObjects;
+import dae.animation.skeleton.BodyElement;
+import static dae.io.SceneSaver.writeAttribute;
+import dae.io.XMLUtils;
 import dae.prefabs.Prefab;
 import dae.prefabs.ui.events.ErrorMessage;
+import java.io.IOException;
+import java.io.Writer;
 
 /**
  *
  * @author Koen
  */
-public class MeshObject extends Prefab {
+public class MeshObject extends Prefab implements BodyElement {
 
     private String meshName;
     private String meshFile;
@@ -41,7 +46,7 @@ public class MeshObject extends Prefab {
             Material boxmat = manager.loadMaterial("Materials/ErrorMaterial.j3m");
             box.setMaterial(boxmat);
             this.attachChild(box);
-            setPivot(new Vector3f(0,-0.25f,0));
+            setPivot(new Vector3f(0, -0.25f, 0));
             GlobalObjects.getInstance().postEvent(new ErrorMessage("Could not load mesh :" + extraInfo));
         }
         super.setName(name);
@@ -88,7 +93,7 @@ public class MeshObject extends Prefab {
 //        mo.setPhysicsMesh(this.getPhysicsMesh());
 //
 //        return mo;
-        return (Prefab)clone();
+        return (Prefab) clone();
     }
 
     public String getMeshFile() {
@@ -110,5 +115,60 @@ public class MeshObject extends Prefab {
         }
         Spatial model = manager.loadModel(meshFile);
         this.attachChild(model);
+    }
+
+    public void attachBodyElement(BodyElement element) {
+        if (element instanceof Node) {
+            this.attachChild((Node) element);
+        }
+    }
+
+    public void reset() {
+    }
+
+    public void write(Writer w, int depth) throws IOException {
+        for (int i = 0; i < depth; ++i) {
+            w.write('\t');
+        }
+        w.write("<prefab ");
+        XMLUtils.writeAttribute(w, "class", this.getClass().getName());
+        XMLUtils.writeAttribute(w, "name", this.getName());
+        XMLUtils.writeAttribute(w, "category", this.getCategory());
+        XMLUtils.writeAttribute(w, "type", this.getType());
+        XMLUtils.writeAttribute(w, "prefix", this.getPrefix());
+        XMLUtils.writeAttribute(w, "offset", this.getOffset());
+        XMLUtils.writeAttribute(w, "translation", this.getLocalTranslation());
+        XMLUtils.writeAttribute(w, "rotation", this.getLocalRotation());
+        XMLUtils.writeAttribute(w, "scale", this.getLocalScale());
+        if (hasPhysicsMesh()) {
+            XMLUtils.writeAttribute(w, "physicsMesh", getPhysicsMesh());
+        }
+        XMLUtils.writeAttribute(w, "shadowmode", getShadowMode().toString());
+        XMLUtils.writeAttribute(w, "mesh", getMeshFile());
+
+
+
+        boolean hasBodyElements = false;
+        for (Spatial child : this.getChildren()) {
+            if (child instanceof BodyElement) {
+                hasBodyElements = true;
+                break;
+            }
+        }
+
+        if (!hasBodyElements) {
+            w.write("/>\n");
+        } else {
+            w.write(">\n");
+            for (Spatial child : this.getChildren()) {
+                if (child instanceof BodyElement) {
+                    ((BodyElement) child).write(w, depth + 1);
+                }
+            }
+            for (int i = 0; i < depth; ++i) {
+                w.write('\t');
+            }
+            w.write("</prefab>\n");
+        }
     }
 }
