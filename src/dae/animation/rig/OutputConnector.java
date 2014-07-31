57329689a99fd6ade22251012bed7fbe6d44c424
samynk <samyn.koen@gmail.com>
2014-07-31 17:43:02 +0200

    An output connector will always be connected to some type of joint. Added a method to clone the connector.

--- src/dae/animation/rig/OutputConnector.java
+++ src/dae/animation/rig/OutputConnector.java
@@ -12,8 +12,33 @@ package dae.animation.rig;
  */
 public interface OutputConnector {
     /**
+     * This method is called to allow the OutputConnector to determine then
+     * necessary objects to connect to.
+     * @param rig the rig with the necessary inputs.
+     */
+    public void initialize(Rig rig);
+    /**
      * Connects an output value to the correct property of the object.
      * @param value the value to set.
      */
     public void setValue(float value);
+    /**
+     * Checks if the output is properly initialized.
+     * @return true if the connector is properly initialized, false otherwise.
+     */
+    public boolean isInitialized();
+     /**
+     * Returs a copy (non initialized) of this output connector.
+     */
+    public OutputConnector cloneConnector();
+    /**
+     * Sets the joint name of this input connector.
+     * @param jointName the joint name.
+     */
+    public void setJointName(String jointName);
+    /**
+     * Returns the joint name of this input connector.
+     * @return the joint name.
+     */
+    public String getJointName();
 }
