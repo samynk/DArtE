84cc9cc785f86b56be4d6dd12b5795a9cd0066a4
samynk <samyn.koen@gmail.com>
2014-07-31 17:41:47 +0200

    General interface for a connector type. This makes it possible for user that create their own connectors that can be connected to a manager class.

---
+++ src/dae/animation/rig/ConnectorType.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dae.animation.rig;
+
+/**
+ * Describes the connector type (a name and a description) and also allows the
+ * specify a panel that can configure the connector.
+ *
+ * @author Koen Samyn
+ */
+public class ConnectorType {
+    /**
+     * A unique id for this connector type.
+     */
+    private String id;
+    /**
+     * The descriptive name for this connector type.
+     */
+    private String name;
+    /**
+     * The description for the connector type.
+     */
+    private String description;
+    /**
+     * The customizer panel for the connector type.
+     */
+    private String customizerPanelClass;
+    
+    /**
+     * Creates a new InputConnectorType object for use in the connector customizer.
+     * @param id The unique id for this connector type.
+     * @param name The descriptive name for this connector type.
+     * @param description The description for the connector type.
+     * @param customizerPanelClass The customizer panel for the connector type.
+     */
+    public ConnectorType(String id, String name, String description, String customizerPanelClass)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.customizerPanelClass = customizerPanelClass;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public String getDescription(){
+        return description;
+    }
+    
+    public String getCustomizerPanelClass(){
+        return customizerPanelClass;
+    }
+    
+    @Override
+    public String toString(){
+        return name;
+    }
+}
