e4f3c8109ff712c713da5258bd89326536b3483d
samynk <samyn.koen@gmail.com>
2014-07-31 17:57:34 +0200

    Added the getClassType method.

--- src/dae/prefabs/parameters/ColorParameter.java
+++ src/dae/prefabs/parameters/ColorParameter.java
@@ -4,6 +4,8 @@
  */
 package dae.prefabs.parameters;
 
+import com.jme3.math.ColorRGBA;
+
 /**
  *
  * @author Koen
@@ -12,5 +14,14 @@ public class ColorParameter extends Parameter {
 
     public ColorParameter(String label, String id) {
         super(label, id);
+    }
+    
+     /**
+     * Gets the class type of this parameter.
+     * @return the class type of the parameter.
+     */
+    @Override
+    public Class getClassType() {
+        return ColorRGBA.class;
     }
 }
