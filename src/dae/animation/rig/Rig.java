6336602a91bf58547518ee41dd0437f758d81c56
samynk <samyn.koen@gmail.com>
2014-07-31 17:44:31 +0200

    Added possibility to define targets with keys inside a hashmap.

--- src/dae/animation/rig/Rig.java
+++ src/dae/animation/rig/Rig.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package dae.animation.rig;
 
 import com.jme3.asset.AssetManager;
@@ -20,21 +19,22 @@ import java.util.HashMap;
 import mlproject.fuzzy.FuzzySystem;
 
 /**
- * A rig provides support for procedural animation for mechanical models.
- * A rig typically is not animated with skinning but has distinct meshes
- * that move indepently without deformations.
+ * A rig provides support for procedural animation for mechanical models. A rig
+ * typically is not animated with skinning but has distinct meshes that move
+ * indepently without deformations.
+ *
  * @author Koen Samyn
  */
-public class Rig extends Prefab implements BodyElement{
+public class Rig extends Prefab implements BodyElement {
+
     private FuzzySystem fuzzySystem = new FuzzySystem("default");
-    private HashMap<String,Spatial> targetMap = new HashMap<String,Spatial>();
-    
     private ArrayList<String> targetKeys = new ArrayList<String>();
-    
+    private HashMap<String, Prefab> targets = new HashMap<String, Prefab>();
+
     /**
      * Create a new Rig.
      */
-    public Rig(){
+    public Rig() {
         super.setName("rig");
         setLayerName("default");
         setCategory("Animation");
@@ -45,16 +45,15 @@ public class Rig extends Prefab implements BodyElement{
     public void create(String name, AssetManager manager, String extraInfo) {
         this.setName(name);
         Material rigMaterial = manager.loadMaterial("Materials/RigMaterial.j3m");
-        DiamondShape ds = new DiamondShape(0.1f,true);
-        Geometry g = new Geometry("rigshape",ds);
+        DiamondShape ds = new DiamondShape(0.1f, true);
+        Geometry g = new Geometry("rigshape", ds);
         g.setMaterial(rigMaterial);
         attachChild(g);
     }
-    
-    
-    
+
     /**
      * Attaches a body element to this rig.
+     *
      * @param element the element to attach.
      */
     public void attachBodyElement(BodyElement element) {
@@ -67,49 +66,65 @@ public class Rig extends Prefab implements BodyElement{
      * Resets the rig to its original state.
      */
     public void reset() {
-        
     }
 
     public void write(Writer w, int depth) throws IOException {
-        
     }
-    
+
+    /**
+     * Returns the fuzzy system that is used by this Rig
+     */
+    public FuzzySystem getFuzzySystem() {
+        return fuzzySystem;
+    }
+
+    public void setFuzzySystem(FuzzySystem system) {
+        this.fuzzySystem = system;
+    }
+
+    // implementation of dictionary prototype.
+    // void addPropertyKey( String key ); 
+    //  void removePropertyKey( String key );
+    // 
+    // int getNrOfPropertyKeys();
+    // int getIndexOfPropertyKey(String key);
+    // String getPropertyKeyAt(int index);
+    // 
+    // void setProperty( String key, T value);
+    // T getProperty( String key );
     /**
      * Get the indexed target.
      */
-    public String getTargetKey(int index){
-        return targetKeys.get(index);
+    
+    public void addTargetKey( String key){
+        targetKeys.add(key);
     }
     
-    /**
-     * Sets a a target key to a specific value.
-     * @param index the index of the element.
-     * @param key the key to put into the element.
-     */
-    public void setTargetKey(int index, String key){
-        this.targetKeys.set(index,key);
+    public void removeTargetKey(String key){
+        targetKeys.remove(key);
+        targets.remove(key);
     }
     
-    /**
-     * Returns the number of target keys.
-     * @return the number of target keys.
-     */
     public int getNrOfTargetKeys(){
         return targetKeys.size();
     }
     
-    /**
-     * Add a target key to the list.
-     * @param key the key to add.
-     */
-    public void addTargetKey(String key){
-        targetKeys.add(key);
+    public int getIndexOfTargetKey(String key){
+        return targetKeys.indexOf(key);
     }
     
-    /**
-     * Returns the fuzzy system that is used by this Rig
-     */
-    public FuzzySystem getFuzzySystem(){
-        return fuzzySystem;
+    public String getTargetKeyAt(int index) {
+        return targetKeys.get(index);
+    }
+
+    public void setTarget(String key, Prefab value){
+        targets.put(key,value);
+        if ( !targetKeys.contains(key)){
+            targetKeys.add(key);
+        }
+    }
+    
+    public Prefab getTarget(String key){
+        return targets.get(key);
     }
 }
