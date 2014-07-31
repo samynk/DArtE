0bf4461da4d72afc47eac0d076c0d0e6c332a494
samynk <samyn.koen@gmail.com>
2014-07-31 17:43:35 +0200

    This defines a callback that is called when a PrefabPlaceHolder has found the actual object.

---
+++ src/dae/animation/rig/PrefabPlaceHolderCallback.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dae.animation.rig;
+
+import dae.prefabs.Prefab;
+import dae.prefabs.standard.PrefabPlaceHolder;
+
+/**
+ * Defines a call back for PrefabPlaceHolder objects.
+ * @author Koen Samyn
+ */
+public interface PrefabPlaceHolderCallback {
+    /**
+     * Called when the actual prefab has been found.
+     * @param actualPrefab the prefab that was found.
+     * @param placeHolder the placeholder object that held the place of the actual prefab.
+     */
+    public void prefabFound(Prefab actualPrefab, PrefabPlaceHolder placeHolder);
+}
