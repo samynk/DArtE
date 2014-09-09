2029b1f21bbfebdba8c8f8d6e34ed2a92e4337c2
samynk <samyn.koen@gmail.com>
2014-09-09 16:54:19 +0200

    Access to documentation

---
+++ src/dae/gui/help/GettingStartedHelp.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dae.gui.help;
+
+/**
+ *
+ * @author Koen Samyn
+ */
+public class GettingStartedHelp {
+
+}
