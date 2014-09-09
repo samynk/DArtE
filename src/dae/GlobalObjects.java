09d7e8d437accf825b7980bb70a853bc58881a74
samynk <samyn.koen@gmail.com>
2014-09-09 12:13:09 +0200

    installCameraKeys method installs the correct keys, based on the user language.

--- src/dae/GlobalObjects.java
+++ src/dae/GlobalObjects.java
@@ -7,6 +7,11 @@ package dae;
 import com.google.common.eventbus.EventBus;
 import com.jme3.asset.AssetManager;
 import com.jme3.input.FlyByCamera;
+import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseAxisTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
@@ -15,6 +20,7 @@ import dae.prefabs.AxisEnum;
 import dae.prefabs.prefab.undo.UndoPrefabPropertyEdit;
 import dae.prefabs.standard.RotationRange;
 import dae.prefabs.types.ObjectTypeCategory;
+import java.awt.im.InputContext;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -23,6 +29,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.prefs.Preferences;
 import javax.swing.undo.CannotRedoException;
 import javax.swing.undo.CannotUndoException;
@@ -36,6 +44,7 @@ import javax.swing.undo.UndoableEdit;
 public class GlobalObjects {
 
     private AssetManager manager;
+    private InputManager inputManager;
     private EventBus eventBus;
     private ObjectTypeCategory objectsToCreate;
     private RotationRange defaultRotationRange;
@@ -272,5 +281,93 @@ public class GlobalObjects {
         } else {
             list.addAll(items);
         }
+    }
+
+    /**
+     * Returns the input manager for the jmonkey application
+     *
+     * @return the input manager.
+     */
+    public InputManager getInputManager() {
+        return inputManager;
+    }
+
+    /**
+     * Sets the input manager for the jmonkey application.
+     *
+     * @param inputManager the input manager.
+     */
+    public void setInputManager(InputManager inputManager) {
+        this.inputManager = inputManager;
+    }
+    String[] cameraKeys = {"FLYCAM_Left",
+        "FLYCAM_Right",
+        "FLYCAM_Up",
+        "FLYCAM_Down",
+        "FLYCAM_StrafeLeft",
+        "FLYCAM_StrafeRight",
+        "FLYCAM_Forward",
+        "FLYCAM_Backward",
+        "FLYCAM_Rise",
+        "FLYCAM_Lower",
+        "FLYCAM_ZoomIn",
+        "FLYCAM_ZoomOut",
+        "FLYCAM_RotateDrag"};
+
+    /**
+     * Installs the defines camera keys for the application.
+     *
+     * @param flyCam the flyby camera to install the keys for.
+     * @param inputManager the inputmanager of the sandbox application.
+     */
+    public void installCameraKeys(FlyByCamera flyCam, InputManager inputManager) {
+        InputContext ic = InputContext.getInstance();
+        Locale l = ic.getLocale();
+        System.out.println("language : " + l.getLanguage());
+        System.out.println("Country : " + l.getCountry());
+        ResourceBundle cameraKeyBundle = ResourceBundle.getBundle("i18n.camerakeys", ic.getLocale());
+        if (cameraKeyBundle == null) {
+            cameraKeyBundle = ResourceBundle.getBundle("camerakeys");
+        }
+
+        String keyStrokeLeft = cameraKeyBundle.getString("FLYCAM_Left");
+        int leftKeyCode = Integer.parseInt(keyStrokeLeft, 16);
+        inputManager.addMapping("FLYCAM_Left", new MouseAxisTrigger(0, true),
+                new KeyTrigger(leftKeyCode));
+
+        String keyStrokeRight = cameraKeyBundle.getString("FLYCAM_Right");
+        int rightKeyCode = Integer.parseInt(keyStrokeRight, 16);
+        inputManager.addMapping("FLYCAM_Right", new MouseAxisTrigger(0, false),
+                new KeyTrigger(rightKeyCode));
+
+        String keyStrokeUp = cameraKeyBundle.getString("FLYCAM_Up");
+        int upKeyCode = Integer.parseInt(keyStrokeUp, 16);
+        inputManager.addMapping("FLYCAM_Up", new MouseAxisTrigger(1, false),
+                new KeyTrigger(upKeyCode));
+
+        String keyStrokeDown = cameraKeyBundle.getString("FLYCAM_Down");
+        int downKeyCode = Integer.parseInt(keyStrokeDown, 16);
+        inputManager.addMapping("FLYCAM_Down", new MouseAxisTrigger(1, true),
+                new KeyTrigger(downKeyCode));
+
+        inputManager.addMapping("FLYCAM_ZoomIn", new MouseAxisTrigger(2, false));
+        inputManager.addMapping("FLYCAM_ZoomOut", new MouseAxisTrigger(2, true));
+        inputManager.addMapping("FLYCAM_RotateDrag", new MouseButtonTrigger(0));
+
+        inputManager.addMapping("FLYCAM_StrafeLeft", getKeyCode("FLYCAM_StrafeLeft", cameraKeyBundle));
+        inputManager.addMapping("FLYCAM_StrafeRight", getKeyCode("FLYCAM_StrafeRight", cameraKeyBundle));
+        inputManager.addMapping("FLYCAM_Forward", getKeyCode("FLYCAM_Forward", cameraKeyBundle));
+        inputManager.addMapping("FLYCAM_Backward", getKeyCode("FLYCAM_Backward", cameraKeyBundle));
+        inputManager.addMapping("FLYCAM_Rise", getKeyCode("FLYCAM_Rise", cameraKeyBundle));
+        inputManager.addMapping("FLYCAM_Lower", getKeyCode("FLYCAM_Lower", cameraKeyBundle));
+
+        inputManager.addListener(flyCam, cameraKeys);
+        flyCam.registerWithInput(inputManager);
+    }
+
+    private KeyTrigger getKeyCode(String key, ResourceBundle bundle) {
+        String sKeyCode = bundle.getString(key);
+        KeyInput ki;
+        return new KeyTrigger(Integer.parseInt(sKeyCode, 16));
     }
 }
