16a9d7e14b8336f80a5d3c20c6ee2e9d4855602c
samynk <samyn.koen@gmail.com>
2014-10-14 15:48:28 +0200

    Added design time support for trigger box physics.

--- src/dae/prefabs/standard/TriggerBox.java
+++ src/dae/prefabs/standard/TriggerBox.java
@@ -1,48 +1,75 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package dae.prefabs.standard;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.bullet.collision.shapes.BoxCollisionShape;
+import com.jme3.bullet.control.GhostControl;
 import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.queue.RenderQueue.Bucket;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 import dae.prefabs.Prefab;
 
 /**
+ * Creates a physics trigger box.
  *
- * @author Koen
+ * @author Koen Samyn
  */
 public class TriggerBox extends Prefab {
 
     private Vector3f dimension;
     private String id;
+    private PhysicsSpace physicsSpace;
 
     public TriggerBox() {
         dimension = new Vector3f(1, 1, 1);
+    }
+
+    /**
+     * Returns the prefix for this trigger box.
+     *
+     * @return the prefix.
+     */
+    @Override
+    public String getPrefix() {
+        return "Trigger";
     }
 
     @Override
     public final void create(String name, AssetManager manager, String extraInfo) {
         this.setName(name);
 
-        Box b = new Box(Vector3f.ZERO, dimension); // create cube shape at the origin
+        Box b = new Box(dimension.mult(-0.5f), dimension.mult(0.5f)); // create cube shape at the origin
         Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
 
-        Material mat = new Material(manager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Orange);
-
+        Material mat = manager.loadMaterial("Materials/TriggerBoxMaterial.j3m");
         setOriginalMaterial(mat);
         geom.setMaterial(mat);
         attachChild(geom);
+
+        this.setQueueBucket(Bucket.Transparent);
     }
 
     @Override
-    public String getPrefix() {
-        return "Trigger";
+    public void addPhysics(PhysicsSpace space) {
+        this.physicsSpace = space;
+        createControl();
+    }
+
+    /**
+     * Creates the control on the basis of the current dimensions.
+     */
+    private void createControl() {
+        GhostControl gc = this.getControl(GhostControl.class);
+        if (gc == null) {
+            gc = new GhostControl(new BoxCollisionShape(dimension.mult(0.5f)));
+            addControl(gc);
+
+        }
+        if (physicsSpace != null && gc.getPhysicsSpace() == null) {
+            physicsSpace.add(gc);
+        }
     }
 
     /**
@@ -64,12 +91,19 @@ public class TriggerBox extends Prefab {
      * @param dimension the dimension to set
      */
     public void setDimension(Vector3f dimension) {
-        Box b = new Box(Vector3f.ZERO, dimension);
+        Box b = new Box(dimension.mult(-0.5f), dimension.mult(0.5f));
         Geometry geom = new Geometry("Box", b);
         geom.setMaterial(this.getOriginalMaterial());
         detachAllChildren();
         attachChild(geom);
         this.dimension = dimension;
+
+        GhostControl gc = this.getControl(GhostControl.class);
+        if (gc != null) {
+            gc.setEnabled(false);
+            removeControl(gc);
+        }
+        createControl();
     }
 
     public void setWidth(float width) {
