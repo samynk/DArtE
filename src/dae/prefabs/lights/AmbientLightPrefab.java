cbf47a06b47916355c776a7e7dfa31cb59a96c68
samynk <samyn.koen@gmail.com>
2014-10-14 14:45:38 +0200

    Removed unnecessary imports.

--- src/dae/prefabs/lights/AmbientLightPrefab.java
+++ src/dae/prefabs/lights/AmbientLightPrefab.java
@@ -6,9 +6,7 @@ package dae.prefabs.lights;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.light.AmbientLight;
-import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Sphere;
