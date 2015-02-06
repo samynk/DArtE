551f53c019a98aa81d7ab35cd0694a6972163e37
samynk <samyn.koen@gmail.com>
2015-02-06 10:49:31 +0100

    Updated documentation. Removed unnecessary import.

--- src/dae/prefabs/parameters/DefaultSection.java
+++ src/dae/prefabs/parameters/DefaultSection.java
@@ -1,10 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package dae.prefabs.parameters;
 
-import dae.prefabs.parameters.Parameter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
