15fb89f913d8841c7339a51584a37544db6f86dd
samynk <samyn.koen@gmail.com>
2015-02-06 09:46:29 +0100

    Removed unneccesary setTranslate functions. Waypoints that are linked to a waypoint are inserted in the parent character path instead.

--- src/dae/animation/custom/Waypoint.java
+++ src/dae/animation/custom/Waypoint.java
@@ -5,7 +5,10 @@
 package dae.animation.custom;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.math.Matrix4f;
+import com.jme3.math.Transform;
 import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
 import dae.prefabs.Prefab;
 
 /**
@@ -16,64 +19,62 @@ import dae.prefabs.Prefab;
  * @author Koen Samyn
  */
 public class Waypoint extends Prefab {
+
     public Waypoint() {
         setLayerName("waypoints");
     }
-   
+
     @Override
     public final void create(String name, AssetManager manager, String extraInfo) {
         this.setName(name);
         this.attachChild(manager.loadModel("Skeleton/Helpers/waypoint.j3o"));
     }
 
-   
-
-    @Override
-    public void setXTranslation(float x) {
-        super.setXTranslation(x); //To change body of generated methods, choose Tools | Templates.
-        if ( getParent() instanceof CharacterPath )
-        {
-            ((CharacterPath)getParent()).createPathMesh();
-        }
-    }
-
-    @Override
-    public void setYTranslation(float y) {
-        super.setYTranslation(y); //To change body of generated methods, choose Tools | Templates.
-        if ( getParent() instanceof CharacterPath )
-        {
-            ((CharacterPath)getParent()).createPathMesh();
-        }
-    }
-
-    @Override
-    public void setZTranslation(float z) {
-        super.setZTranslation(z); //To change body of generated methods, choose Tools | Templates.
-        if ( getParent() instanceof CharacterPath )
-        {
-            ((CharacterPath)getParent()).createPathMesh();
-        }
-    }
-
     @Override
     public void setLocalTranslation(Vector3f localTranslation) {
         super.setLocalTranslation(localTranslation); //To change body of generated methods, choose Tools | Templates.
-        if ( getParent() instanceof CharacterPath )
-        {
-            ((CharacterPath)getParent()).createPathMesh();
+        if (getParent() instanceof CharacterPath) {
+            ((CharacterPath) getParent()).createPathMesh();
         }
     }
 
     @Override
     public void setLocalTranslation(float x, float y, float z) {
         super.setLocalTranslation(x, y, z); //To change body of generated methods, choose Tools | Templates.
-        if ( getParent() instanceof CharacterPath )
-        {
-            ((CharacterPath)getParent()).createPathMesh();
+        if (getParent() instanceof CharacterPath) {
+            ((CharacterPath) getParent()).createPathMesh();
         }
     }
-    
-    
-    
-    
+
+    @Override
+    public int attachChild(Spatial child) {
+        if (getParent() instanceof CharacterPath && child instanceof Waypoint) {
+            Waypoint w = (Waypoint) child;
+            // local transform is relative to this transform.
+            // needs to be relative to parent.
+            Matrix4f parentMatrix = new Matrix4f();
+            parentMatrix.setTranslation(getParent().getWorldTranslation());
+            parentMatrix.setRotationQuaternion(getParent().getWorldRotation());
+            parentMatrix.setScale(getParent().getWorldScale());
+            parentMatrix.invertLocal();
+
+            Matrix4f childMatrix = new Matrix4f();
+            childMatrix.setTranslation(child.getWorldTranslation());
+            childMatrix.setRotationQuaternion(child.getWorldRotation());
+            childMatrix.setScale(child.getWorldScale());
+
+            Matrix4f local = parentMatrix.mult(childMatrix);
+
+            child.setLocalTranslation(local.toTranslationVector());
+            child.setLocalRotation(local.toRotationQuat());
+            child.setLocalScale(local.toScaleVector());
+
+            ((CharacterPath) getParent()).insertWaypointAfter(this, w);
+
+            // child is not attached to this node.
+            return -1;
+        } else {
+            return super.attachChild(child);
+        }
+    }
 }
