501411fe825a5bd729f895fa5f2b81d1e3062fd0
samynk <samyn.koen@gmail.com>
2015-02-06 10:39:10 +0100

    Interface for level exporters.

---
+++ src/dae/io/writers/Exporter.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dae.io.writers;
+
+import com.jme3.asset.AssetManager;
+import dae.project.Level;
+import java.io.File;
+
+/**
+ *
+ * @author Koen Samyn
+ */
+public interface Exporter {
+    /**
+     * Write the scene to a file.
+     *
+     * @param location The location of the file.
+     * @param node the root node of the scene.
+     */
+    public void writeScene(File location, AssetManager manager, Level level);
+}
