162be5f066819e03864795a49d549b00635d35b3
samynk <samyn.koen@gmail.com>
2015-02-06 09:47:24 +0100

    Joint with a tendon simulation.

---
+++ src/dae/animation/rig/TendonJoint.java
@@ -0,0 +1,9 @@
+package dae.animation.rig;
+/**
+ * Describes a joint with a simplified tendon system.
+ * The tendon system allows an easy definition of constraints.
+ * @author Koen Samyn
+ */
+public class TendonJoint {
+
+}
