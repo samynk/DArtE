6441c82f1ae87301fddd093dec70fd6fbcc5093b
samynk <samyn.koen@gmail.com>
2016-01-26 11:22:22 +0100

    implemented deinstall method added game install method.

--- src/dae/components/AnimationComponent.java
+++ src/dae/components/AnimationComponent.java
@@ -3,6 +3,7 @@ package dae.components;
 import com.jme3.animation.AnimChannel;
 import com.jme3.animation.AnimControl;
 import com.jme3.asset.AssetManager;
+import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import dae.GlobalObjects;
 import dae.animation.AnimationSet;
@@ -17,7 +18,7 @@ public class AnimationComponent extends PrefabComponent {
 
     private String animationSet;
     private String animation;
-    private Prefab toAnimate;
+    private Spatial toAnimate;
     private AnimationSet animations;
     private AnimChannel animationChannel;
     private ArrayList<String> animationNames = new ArrayList<String>();
@@ -26,6 +27,39 @@ public class AnimationComponent extends PrefabComponent {
     public void install(Prefab parent) {
         toAnimate = parent;
         loadAnimationSet();
+    }
+
+    @Override
+    public void installGameComponent(Spatial parent) {
+        toAnimate = parent;
+        loadAnimationSet();
+        if (animation != null) {
+            setAnimation(animation);
+        }
+    }
+
+    /**
+     * Removes this AnimationComponent from the parent.
+     *
+     * @param parent the parent to remove the AnimationComponent from.
+     */
+    @Override
+    public void deinstall() {
+        if (animationSet == null || toAnimate == null) {
+            return;
+        }
+        AnimControl ac = toAnimate.getControl(AnimControl.class);
+        if (ac != null) {
+            ac.clearChannels();
+        } else if (toAnimate instanceof Node) {
+            Node parent = (Node) toAnimate;
+            for (Spatial child : parent.getChildren()) {
+                AnimControl cac = child.getControl(AnimControl.class);
+                if (cac != null) {
+                    cac.clearChannels();
+                }
+            }
+        }
     }
 
     /**
@@ -79,8 +113,7 @@ public class AnimationComponent extends PrefabComponent {
      */
     public void setAnimation(String animation) {
         this.animation = animation;
-        if ( animationChannel != null)
-        {
+        if (animationChannel != null) {
             animationChannel.setAnim(animation);
         }
     }
@@ -97,21 +130,31 @@ public class AnimationComponent extends PrefabComponent {
             AssetManager manager = GlobalObjects.getInstance().getAssetManager();
             animations = (AnimationSet) manager.loadAsset(animationSet);
 
-            for (Spatial child : toAnimate.getChildren()) {
-                AnimControl ac = child.getControl(AnimControl.class);
-                if (ac != null) {
-                    // clear the previous animations
-                    ac.clearChannels();
-                    animationNames.clear();
-                    // set the new ones.
-                    for (Spatial s : animations.getAnimations()) {
-                        AnimControl controlToCopy = s.getControl(AnimControl.class);
-                        copyAnimControl(child, controlToCopy);
+            AnimControl ac = toAnimate.getControl(AnimControl.class);
+            if (ac != null) {
+                initializeAnimationChannel(ac, toAnimate);
+            } else if (toAnimate instanceof Node) {
+                Node parent = (Node) toAnimate;
+                for (Spatial child : parent.getChildren()) {
+                    AnimControl cac = child.getControl(AnimControl.class);
+                    if (cac != null) {
+                        initializeAnimationChannel(cac, child);
                     }
-                    animationChannel = initAnimation(child);
-                    animationChannel.setAnim("idle1");
                 }
             }
         }
+    }
+
+    private void initializeAnimationChannel(AnimControl ac, Spatial child) {
+        // clear the previous animations
+        ac.clearChannels();
+        animationNames.clear();
+        // set the new ones.
+        for (Spatial s : animations.getAnimations()) {
+            AnimControl controlToCopy = s.getControl(AnimControl.class);
+            copyAnimControl(child, controlToCopy);
+        }
+        animationChannel = initAnimation(child);
+        setAnimation("idle1");
     }
 }
