1f2f894f05c1be58341fe7838b4586037f7b9862
samynk <samyn.koen@gmail.com>
2016-01-26 11:56:58 +0100

    Refactored to make it easier to replace the demo objects.

--- src/dae/prefabs/physics/JointPrefab.java
+++ src/dae/prefabs/physics/JointPrefab.java
@@ -8,13 +8,10 @@ import com.jme3.math.FastMath;
 import com.jme3.math.Transform;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
-import dae.GlobalObjects;
 import dae.animation.event.TransformListener;
 import dae.animation.event.TransformType;
 import dae.prefabs.Prefab;
 import dae.prefabs.shapes.HingeShape;
-import dae.prefabs.standard.CrateObject;
-import dae.prefabs.types.ObjectType;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
@@ -48,32 +45,24 @@ public class JointPrefab extends Prefab implements PropertyChangeListener, Trans
     }
 
     @Override
-    public void create(String name, AssetManager manager, String extraInfo) {
-        this.setName(name);
-
-
+    public void create(AssetManager manager, String extraInfo) {
         // two example objects are temporarily added to this node.
         // once the hinge joint is added to the scene the children will be detached
         // and added to main scene.
-        CrateObject co1 = new CrateObject(new Vector3f(.1f, .1f, .1f));
-        ObjectType type = GlobalObjects.getInstance().getObjectsTypeCategory().getObjectType("Standard", "Crate");
-        co1.create("Object A", manager, type, null);
-        co1.setType("Crate");
-        co1.setCategory("Standard");
-        this.objectA = co1;
-        objectAPivot = new Vector3f(0.5f, 0, 0);
-        co1.setLocalTranslation(objectAPivot.negate());
-
-        attachChild(co1);
-
-        CrateObject co2 = new CrateObject(new Vector3f(.6f, 0.9f, .05f));
-        co2.create("Object B", manager, type, null);
-        co2.setType("Crate");
-        co2.setCategory("Standard");
-        this.objectB = co2;
-        objectBPivot = new Vector3f(-0.3f, 0, 0);
-        co2.setLocalTranslation(objectBPivot.negate());
-        attachChild(co2);
+       
+//        ObjectType type = GlobalObjects.getInstance().getObjectsTypeCategory().getObjectType("Standard", "Crate");
+//        CrateObject co1 = (CrateObject)type.create(manager, "Object A");
+//        this.setObjectA(co1);
+//        setObjectAPivot(new Vector3f(1.5f, 0, 0));
+//        co1.setLocalTranslation(getObjectAPivot().negate());
+//
+//        attachChild(co1);
+//
+//        CrateObject co2 = (CrateObject)type.create(manager, "Object B");
+//        this.setObjectB(co2);
+//        setObjectBPivot(new Vector3f(-0.5f, 0, 0));
+//        co2.setLocalTranslation(getObjectBPivot().negate());
+//        attachChild(co2);
 
         hingeShape = new HingeShape(this.axis, this.lowerLimit, this.upperLimit);
         hingeShape.create(manager);
@@ -84,22 +73,22 @@ public class JointPrefab extends Prefab implements PropertyChangeListener, Trans
     public void addPhysics(PhysicsSpace space) {
         this.space = space;
         Node scene = this.getParent();
-        Transform aTransform = objectA.getWorldTransform();
-        scene.attachChild(objectA);
-        objectA.setLocalTransform(aTransform);
+        Transform aTransform = getObjectA().getWorldTransform();
+        scene.attachChild(getObjectA());
+        getObjectA().setLocalTransform(aTransform);
 
-        Transform bTransform = objectB.getWorldTransform();
-        scene.attachChild(objectB);
-        objectB.setLocalTransform(bTransform);
+        Transform bTransform = getObjectB().getWorldTransform();
+        scene.attachChild(getObjectB());
+        getObjectB().setLocalTransform(bTransform);
 
-        objectA.addPhysics(space, 0);
-        objectB.addPhysics(space);
-        RigidBodyControl rbcB = objectB.getControl(RigidBodyControl.class);
+        getObjectA().addPhysics(space, 0);
+        getObjectB().addPhysics(space);
+        RigidBodyControl rbcB = getObjectB().getControl(RigidBodyControl.class);
         rbcB.setGravity(Vector3f.ZERO);
 
 
 
-        joint = new HingeJoint(objectA.getControl(RigidBodyControl.class), objectB.getControl(RigidBodyControl.class), objectAPivot, objectBPivot, axis, axis);
+        joint = new HingeJoint(getObjectA().getControl(RigidBodyControl.class), getObjectB().getControl(RigidBodyControl.class), getObjectAPivot(), getObjectBPivot(), axis, axis);
         joint.setAngularOnly(false);
         updateJoint();
         space.add(joint);
@@ -110,15 +99,19 @@ public class JointPrefab extends Prefab implements PropertyChangeListener, Trans
     public void turnCW() {
 //        PhysicsControl pc = objectB.getControl(PhysicsControl.class);
 //        pc.setEnabled(true);
-        objectB.getControl(RigidBodyControl.class).activate();
+        getObjectB().getControl(RigidBodyControl.class).activate();
         joint.enableMotor(true, velocity, getMaxMotorImpulse());
     }
 
     public void turnCCW() {
 //        PhysicsControl pc = objectB.getControl(PhysicsControl.class);
 //        pc.setEnabled(true);
-        objectB.getControl(RigidBodyControl.class).activate();
-        joint.enableMotor(true, -velocity, getMaxMotorImpulse());
+        RigidBodyControl rbcB = getObjectB().getControl(RigidBodyControl.class);
+        if ( rbcB != null)
+        {
+            rbcB.activate();
+            joint.enableMotor(true, -velocity, getMaxMotorImpulse());
+        }
     }
 
     public void stop() {
@@ -249,35 +242,103 @@ public class JointPrefab extends Prefab implements PropertyChangeListener, Trans
         //if (type == TransformType.TRANSLATION || type == Transform) {
         // update the location of this node.
         Vector3f aPivot = joint.getPivotA();
-        Vector3f aPivotWorld = objectA.localToWorld(aPivot, null);
+        Vector3f aPivotWorld = getObjectA().localToWorld(aPivot, null);
         this.setLocalTranslation(aPivotWorld);
-        objectB.getControl(RigidBodyControl.class).activate();
+        getObjectB().getControl(RigidBodyControl.class).activate();
         //}
         
-        this.setLocalRotation(objectA.getWorldRotation());
+        this.setLocalRotation(getObjectA().getWorldRotation());
     }
 
     @Override
     public void translationChanged() {
         if (addedToScene) {
             Vector3f world = this.getWorldTranslation();
-            objectAPivot = objectA.worldToLocal(world, null);
+            setObjectAPivot(getObjectA().worldToLocal(world, null));
             recreateJoint();
         }
     }
 
     private void recreateJoint() {
+        if ( space == null || objectA == null || objectB == null )
+            return;
         if (joint != null) {
             space.remove(joint);
             joint.destroy();
         }
+        RigidBodyControl rbcA = getObjectA().getControl(RigidBodyControl.class);
+        RigidBodyControl rbcB = getObjectB().getControl(RigidBodyControl.class);
+        if ( rbcA == null || rbcB == null)
+            return;
         joint = new HingeJoint(
-                objectA.getControl(RigidBodyControl.class),
-                objectB.getControl(RigidBodyControl.class),
-                new Vector3f(objectAPivot),
-                new Vector3f(objectBPivot),
+                getObjectA().getControl(RigidBodyControl.class),
+                getObjectB().getControl(RigidBodyControl.class),
+                new Vector3f(getObjectAPivot()),
+                new Vector3f(getObjectBPivot()),
                 axis, axis);
         updateJoint();
         this.space.add(joint);
+    }
+
+    /**
+     * @return the objectA
+     */
+    public Prefab getObjectA() {
+        return objectA;
+    }
+
+    /**
+     * @param objectA the objectA to set
+     */
+    public void setObjectA(Prefab objectA) {
+        this.objectA = objectA;
+        if (this.objectA != null){
+            recreateJoint();
+        }
+    }
+
+    /**
+     * @return the objectB
+     */
+    public Prefab getObjectB() {
+        return objectB;
+    }
+
+    /**
+     * @param objectB the objectB to set
+     */
+    public void setObjectB(Prefab objectB) {
+        this.objectB = objectB;
+        if (this.objectB != null){
+            recreateJoint();
+        }
+    }
+
+    /**
+     * @return the objectAPivot
+     */
+    public Vector3f getObjectAPivot() {
+        return objectAPivot;
+    }
+
+    /**
+     * @param objectAPivot the objectAPivot to set
+     */
+    public void setObjectAPivot(Vector3f objectAPivot) {
+        this.objectAPivot = objectAPivot;
+    }
+
+    /**
+     * @return the objectBPivot
+     */
+    public Vector3f getObjectBPivot() {
+        return objectBPivot;
+    }
+
+    /**
+     * @param objectBPivot the objectBPivot to set
+     */
+    public void setObjectBPivot(Vector3f objectBPivot) {
+        this.objectBPivot = objectBPivot;
     }
 }
