a4ee969bfa58ee60792a3e9e42b76935f56e958e
samynk <samyn.koen@gmail.com>
2016-01-26 11:46:55 +0100

    Added parameter to the interface method.

--- src/dae/io/readers/ParameterParser.java
+++ src/dae/io/readers/ParameterParser.java
@@ -1,14 +1,18 @@
 package dae.io.readers;
 
+import dae.prefabs.parameters.Parameter;
+
 /**
  * 
  * @author Koen Samyn
  */
 public interface ParameterParser {
     /**
-     * Converts a string to a value.
+     * Converts a string to a value.   
+     * @param parent the object that is the parent of this object.
+     * @param parameter the parameter that is associated with the value.
      * @param value the value to parse.
-     * @return 
+     * @return the parsed value.
      */
-    Object parseParameter(String value);
+    Object parseParameter(Object parent, Parameter parameter, String value);
 }
