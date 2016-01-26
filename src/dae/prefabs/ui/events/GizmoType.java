5d5a7de9c4cbed6cf5485de0b46cdcd406f8821e
samynk <samyn.koen@gmail.com>
2016-01-26 12:13:15 +0100

    Added support for BRUSH and IDLE state.

--- src/dae/prefabs/ui/events/GizmoType.java
+++ src/dae/prefabs/ui/events/GizmoType.java
@@ -1,13 +1,9 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package dae.prefabs.ui.events;
 
 /**
- *
- * @author samyn_000
+ * Defines the gizmo types.
+ * @author Koen Samyn
  */
 public enum GizmoType {
-    LINK,TRANSLATE,ROTATE,PICK;
+    LINK,TRANSLATE,ROTATE,PICK, BRUSH,NONE;
 }
