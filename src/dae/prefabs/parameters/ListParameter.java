c958d531cba98428652f58dc272794b46239cae0
samynk <samyn.koen@gmail.com>
2016-01-26 11:55:20 +0100

    Added methods to manipulate list elements.

--- src/dae/prefabs/parameters/ListParameter.java
+++ src/dae/prefabs/parameters/ListParameter.java
@@ -1,17 +1,23 @@
 package dae.prefabs.parameters;
 
+import dae.GlobalObjects;
 import dae.components.ComponentType;
+import dae.prefabs.CreateListItemTask;
+import dae.prefabs.Prefab;
+import dae.prefabs.PropertyReflector;
+import dae.prefabs.ReflectionManager;
+import dae.prefabs.types.ObjectType;
 
 /**
  * An indexed parameter defines an indexed property, in other words a property
  * that is backed by an arraylist.
  *
- * If the id of the parameter is property and the type of the property is Type,
+ * If the id of the parameter is "property" and the type of the property is Type,
  * then the following methods must be present.
  *
- * public Type getProperty(int index); public void setProperty(int index, Type
- * key); public int getNrOfTargetKeys(); public void addTargetKey(String key);
- * public void removeTargetKey(String key);
+ * public Type getProperty(int index); 
+ * public void setProperty(int index, Type value); 
+ * public int getPropertySize();
  *
  * @author Koen Samyn
  */
@@ -39,5 +45,62 @@ public class ListParameter extends Parameter {
      */
     public Parameter getBaseType() {
         return baseType;
+    }
+
+    /**
+     * Creates a new empty list item.
+     *
+     * @param prefab the prefab to create a list item for.
+     * @return the newly created object.
+     */
+    public void createListItem(Prefab prefab) {
+        String toCreate = baseType.getType();
+        String[] cs = toCreate.split("\\.");
+        if (cs.length == 2) {
+            String category = cs[0];
+            String type = cs[1];
+            ObjectType ot = GlobalObjects.getInstance().getObjectsTypeCategory().getObjectType(category, type);
+            prefab.addTask(new CreateListItemTask(prefab,this, ot));
+        }
+    }
+    
+    /**
+     * Invokes the get method of this parameter on the prefab.The ComponentType
+     * will be taken into account to invoke this get method on the correct
+     * component.
+     *
+     * @param prefab the prefab to invoke the get method on.
+     * @return the current value of the property.
+     */
+    public Object invokeGet(Prefab prefab) {
+        Object base = prefab;
+        if (getComponentType() != ComponentType.PREFAB) {
+            base = prefab.getComponent(getComponentType().getId());
+        }
+        PropertyReflector pr = ReflectionManager.getInstance().getPropertyReflector(base.getClass());
+        return pr.invokeGetMethod(base, getProperty() + "s");
+    }
+
+    /**
+     * Gets the size of the backing list.
+     *
+     * @return
+     */
+    public int getListSize(Prefab prefab) {
+        ReflectionManager rm = ReflectionManager.getInstance();
+        PropertyReflector pr = rm.getPropertyReflector(prefab.getClass());
+        return pr.invokeListSizeMethod(prefab, getProperty());
+    }
+
+    public Object getListElementAt(Prefab prefab, int index) {
+        ReflectionManager rm = ReflectionManager.getInstance();
+        PropertyReflector pr = rm.getPropertyReflector(prefab.getClass());
+        return pr.invokeListElementAt(prefab, getProperty(), index);
+    }
+
+    public void addListItem(Prefab prefab, Object object) {
+        ReflectionManager rm = ReflectionManager.getInstance();
+        PropertyReflector pr = rm.getPropertyReflector(prefab.getClass());
+        pr.invokeListAddItem(prefab, getProperty(), object);
     }
 }
