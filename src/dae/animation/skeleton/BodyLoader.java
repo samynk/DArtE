eab4bd5d333047f8f0c733f567082f07674be443
samynk <samyn.koen@gmail.com>
2016-01-26 11:19:00 +0100

    Objects are created via ObjectType now.

--- src/dae/animation/skeleton/BodyLoader.java
+++ src/dae/animation/skeleton/BodyLoader.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package dae.animation.skeleton;
 
 import com.jme3.asset.AssetInfo;
@@ -408,9 +404,9 @@ public class BodyLoader implements AssetLoader {
         Vector3f location = parseVector3f(slocation);
         Vector3f rotation = parseVector3f(srotation);
 
-        Handle result = new Handle();
+        
         ObjectType type = GlobalObjects.getInstance().getObjectsTypeCategory().getObjectType("Animation", "TwoAxisHandle");
-        result.create(sname, this.assetManager, type, null);
+        Handle result = (Handle)type.create( this.assetManager ,sname);
         result.setTransformation(location, rotation);
         return result;
     }
