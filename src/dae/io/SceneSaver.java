68f5524fe020a95bf5bb48237f0e40f12ed012b4
samynk <samyn.koen@gmail.com>
2017-10-17 09:50:19 +0200

    Further migration to standardized way of writing prefabs.

--- src/dae/io/SceneSaver.java
+++ src/dae/io/SceneSaver.java
@@ -6,8 +6,6 @@ import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import dae.GlobalObjects;
-import dae.animation.custom.CharacterPath;
-import dae.animation.custom.Waypoint;
 import dae.animation.skeleton.Body;
 import dae.components.ComponentType;
 import dae.components.PrefabComponent;
@@ -17,25 +15,18 @@ import dae.prefabs.Klatch;
 import dae.prefabs.Prefab;
 import dae.prefabs.brush.BrushBatch;
 import dae.prefabs.gizmos.PivotGizmo;
-import dae.prefabs.lights.AmbientLightPrefab;
-import dae.prefabs.lights.DirectionalLightPrefab;
-import dae.prefabs.lights.PointLightPrefab;
-import dae.prefabs.lights.SpotLightPrefab;
-import dae.prefabs.standard.CameraEntity;
 import dae.prefabs.standard.J3ONPC;
 import dae.prefabs.standard.NPCLocationEntity;
 import dae.prefabs.standard.NavigationMesh;
-import dae.prefabs.standard.PlayerStartEntity;
 import dae.prefabs.standard.SituationEntity;
-import dae.prefabs.standard.SoundEntity;
 import dae.prefabs.standard.Terrain;
 import dae.prefabs.standard.TriggerBox;
-import dae.prefabs.standard.WayPointEntity;
 import dae.prefabs.types.ObjectType;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -136,7 +127,7 @@ public class SceneSaver {
         return helper.toString();
     }
 
-    public static String writeEndTag(BufferedWriter bw, String tag, Prefab p, int depth) throws IOException {
+    public static String writeEndTag(Writer bw, String tag, Prefab p, int depth) throws IOException {
         if (!p.hasChildren() && !p.hasComponents() && !p.getObjectType().hasParameters()) {
             bw.write("/>\n");
             return "";
@@ -146,7 +137,7 @@ public class SceneSaver {
         }
     }
 
-    public static void writeAttribute(BufferedWriter bw, String key, String value) throws IOException {
+    public static void writeAttribute(Writer bw, String key, String value) throws IOException {
         if (value == null || value.length() == 0) {
             return;
         }
@@ -156,28 +147,28 @@ public class SceneSaver {
         bw.write("' ");
     }
 
-    public static void writeAttribute(BufferedWriter bw, String key, float value) throws IOException {
+    public static void writeAttribute(Writer bw, String key, float value) throws IOException {
         bw.write(key);
         bw.write("='");
         bw.write(Float.toString(value));
         bw.write("' ");
     }
 
-    public static void writeAttribute(BufferedWriter bw, String key, int value) throws IOException {
+    public static void writeAttribute(Writer bw, String key, int value) throws IOException {
         bw.write(key);
         bw.write("='");
         bw.write(Integer.toString(value));
         bw.write("' ");
     }
 
-    public static void writeAttribute(BufferedWriter bw, String key, boolean value) throws IOException {
+    public static void writeAttribute(Writer bw, String key, boolean value) throws IOException {
         bw.write(key);
         bw.write("='");
         bw.write(Boolean.toString(value));
         bw.write("' ");
     }
 
-    public static void writeAttribute(BufferedWriter bw, String key, Vector3f value) throws IOException {
+    public static void writeAttribute(Writer bw, String key, Vector3f value) throws IOException {
         if (value == null) {
             return;
         }
@@ -191,7 +182,7 @@ public class SceneSaver {
         bw.write("]' ");
     }
 
-    public static void writeAttribute(BufferedWriter bw, String key, Quaternion value) throws IOException {
+    public static void writeAttribute(Writer bw, String key, Quaternion value) throws IOException {
         if (value == null) {
             return;
         }
@@ -207,7 +198,7 @@ public class SceneSaver {
         bw.write("]' ");
     }
 
-    public static void writeAttribute(BufferedWriter bw, String key, ColorRGBA value) throws IOException {
+    public static void writeAttribute(Writer bw, String key, ColorRGBA value) throws IOException {
         if (value == null) {
             return;
         }
@@ -223,22 +214,7 @@ public class SceneSaver {
         bw.write("]' ");
     }
 
-    private static String writeSinglePrefab(BufferedWriter bw, Spatial child, int depth) throws IOException {
-        XMLUtils.writeTabs(bw, depth);
-        bw.write("<prefab ");
-        Prefab p = (Prefab) child;
-        writeAttribute(bw, "class", p.getClass().getName());
-        writeAttribute(bw, "name", p.getName());
-        writeAttribute(bw, "category", p.getCategory());
-        writeAttribute(bw, "type", p.getType());
-        writeAttribute(bw, "prefix", p.getPrefix());
-        writeAttribute(bw, "offset", p.getOffset());
-
-        writeAttribute(bw, "shadowmode", p.getShadowMode().toString());
-        return writeEndTag(bw, "prefab", p, depth);
-    }
-
-    private static String writeKlatch(Klatch k, BufferedWriter bw, int depth) throws IOException {
+    private static String writeKlatch(Klatch k, Writer bw, int depth) throws IOException {
         XMLUtils.writeTabs(bw, depth);
         bw.write("<klatch ");
         writeAttribute(bw, "name", k.getName());
@@ -255,7 +231,7 @@ public class SceneSaver {
         return writeEndTag(bw, "klatch", k, depth);
     }
 
-    private static String writeBody(BufferedWriter bw, Spatial child, int depth) throws IOException {
+    private static String writeBody(Writer bw, Spatial child, int depth) throws IOException {
         XMLUtils.writeTabs(bw, depth);
         bw.write("<body ");
         Body b = (Body) child;
@@ -270,7 +246,7 @@ public class SceneSaver {
         return writeEndTag(bw, "body", b, depth);
     }
 
-    private static String writeJ3ONPC(BufferedWriter bw, Spatial child, int depth) throws IOException {
+    private static String writeJ3ONPC(Writer bw, Spatial child, int depth) throws IOException {
         XMLUtils.writeTabs(bw, depth);
         bw.write("<j3onpc ");
         J3ONPC npc = (J3ONPC) child;
@@ -290,18 +266,7 @@ public class SceneSaver {
         return writeEndTag(bw, "j3onpc", npc, depth);
     }
 
-    private static String writeCameraEntity(BufferedWriter bw, Spatial child, int depth) throws IOException {
-        XMLUtils.writeTabs(bw, depth);
-        bw.write("<camera ");
-        writeAttribute(bw, "name", child.getName());
-        writeAttribute(bw, "category", "Standard");
-        writeAttribute(bw, "type", "Camera");
-        writeAttribute(bw, "cameraid", ((CameraEntity) child).getCameraId());
-        writeAttribute(bw, "startcam", ((CameraEntity) child).getStartCam());
-        return writeEndTag(bw, "camera", (Prefab) child, depth);
-    }
-
-    private static String writeSituationEntity(BufferedWriter bw, Spatial child, int depth) throws IOException {
+    private static String writeSituationEntity(Writer bw, Spatial child, int depth) throws IOException {
         XMLUtils.writeTabs(bw, depth);
         bw.write("<situation ");
         writeAttribute(bw, "class", child.getClass().getName());
@@ -314,76 +279,7 @@ public class SceneSaver {
         return writeEndTag(bw, "situation", (Prefab) child, depth);
     }
 
-    private static String writeSoundEntity(BufferedWriter bw, Spatial child, int depth) throws IOException {
-        XMLUtils.writeTabs(bw, depth);
-        bw.write("<sound ");
-        SoundEntity se = (SoundEntity) child;
-        writeAttribute(bw, "class", child.getClass().getName());
-        writeAttribute(bw, "name", child.getName());
-        writeAttribute(bw, "category", "Standard");
-        writeAttribute(bw, "soundfile", ((SoundEntity) child).getSoundFile());
-        writeAttribute(bw, "type", "Sound");
-        writeAttribute(bw, "positional", se.getPositional());
-        writeAttribute(bw, "looping", se.getLooping());
-        writeAttribute(bw, "volume", se.getVolume());
-        writeAttribute(bw, "translation", child.getLocalTranslation());
-        writeAttribute(bw, "rotation", child.getLocalRotation());
-        writeAttribute(bw, "refDistance", se.getRefDistance());
-        writeAttribute(bw, "maxDistance", se.getMaxDistance());
-        return writeEndTag(bw, "sound", se, depth);
-    }
-
-    private static String writePlayerStartEntity(BufferedWriter bw, Spatial child, int depth) throws IOException {
-        XMLUtils.writeTabs(bw, depth);
-        bw.write("<playerstart ");
-        writeAttribute(bw, "class", child.getClass().getName());
-        writeAttribute(bw, "name", child.getName());
-        writeAttribute(bw, "category", "Standard");
-        writeAttribute(bw, "type", "PlayerStart");
-        writeAttribute(bw, "translation", child.getLocalTranslation());
-        writeAttribute(bw, "rotation", child.getLocalRotation());
-        return writeEndTag(bw, "playerstart", (Prefab) child, depth);
-    }
-
-    private static String writeWaypointEntity(BufferedWriter bw, Spatial child, int depth) throws IOException {
-        XMLUtils.writeTabs(bw, depth);
-        bw.write("<waypoint ");
-        writeAttribute(bw, "class", child.getClass().getName());
-        writeAttribute(bw, "name", child.getName());
-        writeAttribute(bw, "waypointid", ((WayPointEntity) child).getWaypointId());
-        writeAttribute(bw, "category", "Standard");
-        writeAttribute(bw, "type", "Waypoint");
-        writeAttribute(bw, "translation", child.getLocalTranslation());
-        writeAttribute(bw, "rotation", child.getLocalRotation());
-        return writeEndTag(bw, "waypoint", (Prefab) child, depth);
-    }
-
-    private static String writeTriggerBoxEntity(BufferedWriter bw, Spatial child, int depth) throws IOException {
-        XMLUtils.writeTabs(bw, depth);
-        bw.write("<trigger ");
-        TriggerBox box = (TriggerBox) child;
-        writeAttribute(bw, "id", box.getId());
-        writeAttribute(bw, "translation", child.getLocalTranslation());
-        writeAttribute(bw, "dimension", box.getDimension());
-        writeAttribute(bw, "rotation", child.getLocalRotation());
-        writeAttribute(bw, "triggerid", box.getId());
-        writeAttribute(bw, "category", "Standard");
-        writeAttribute(bw, "type", "Trigger");
-        return writeEndTag(bw, "trigger", box, depth);
-    }
-
-    private static String writeNPCLocationEntity(Spatial child, BufferedWriter bw, int depth) throws IOException {
-        XMLUtils.writeTabs(bw, depth);
-        NPCLocationEntity npcloc = (NPCLocationEntity) child;
-        bw.write("<npclocation ");
-        writeAttribute(bw, "name", npcloc.getName());
-        writeAttribute(bw, "rotation", npcloc.getLocalRotation());
-        writeAttribute(bw, "translation", child.getLocalTranslation());
-        writeAttribute(bw, "animation", npcloc.getAnimation());
-        writeAttribute(bw, "npcmesh", npcloc.getNpc());
-        return writeEndTag(bw, "npclocation", npcloc, depth);
-    }
-
+    /*
     private static String writeNavigationMesh(Spatial child, BufferedWriter bw, int depth) throws IOException {
         XMLUtils.writeTabs(bw, depth);
         NavigationMesh nm = (NavigationMesh) child;
@@ -407,9 +303,9 @@ public class SceneSaver {
         writeAttribute(bw, "contoursampledistance", nm.getContourSampleDistance());
         writeAttribute(bw, "contourmaxdeviation", nm.getContourMaxDeviation());
         return writeEndTag(bw, "navmesh", nm, depth);
-    }
+    }*/
 
-    private static String writePivot(Spatial child, BufferedWriter bw, int depth) throws IOException {
+    private static String writePivot(Spatial child, Writer bw, int depth) throws IOException {
         XMLUtils.writeTabs(bw, depth);
         PivotGizmo pg = (PivotGizmo) child;
         bw.write("<pivot ");
@@ -419,59 +315,26 @@ public class SceneSaver {
         return writeEndTag(bw, "pivot", pg, depth);
     }
 
-    private static String writeTerrain(Terrain terrain, BufferedWriter bw, int depth) throws IOException {
-        XMLUtils.writeTabs(bw, depth);
-        bw.write("<terrain ");
-        writeAttribute(bw, "name", terrain.getName());
-        writeAttribute(bw, "texture1", terrain.getTexture1());
-        writeAttribute(bw, "tex1scale", terrain.getTex1Scale());
-        writeAttribute(bw, "texture2", terrain.getTexture2());
-        writeAttribute(bw, "tex2scale", terrain.getTex1Scale());
-        writeAttribute(bw, "texture3", terrain.getTexture3());
-        writeAttribute(bw, "tex3scale", terrain.getTex1Scale());
-        writeAttribute(bw, "alphamap", terrain.getAlphaMap());
-        writeAttribute(bw, "heightmap", terrain.getHeightMap());
-
-        return writeEndTag(bw, "terrain", terrain, depth);
-
-    }
-
-    private static String writeBrushBatch(BrushBatch brushBatch, BufferedWriter bw, int depth) throws IOException {
+    private static String writeBrushBatch(BrushBatch brushBatch, Writer bw, int depth) throws IOException {
         XMLUtils.writeTabs(bw, depth);
         bw.write("<brushbatch ");
         writeAttribute(bw, "creategeometrybatch", brushBatch.getCreateGeometryBatch());
         return writeEndTag(bw, "brushbatch", brushBatch, depth);
     }
 
-    private static void writePrefab(Prefab child, BufferedWriter bw, int depth) throws IOException {
+    public static void writePrefab(Prefab child, Writer bw, int depth) throws IOException {
         boolean hasChildren = child.hasSavableChildren();
         String endtag = "";
         if (child instanceof Body) {
             endtag = writeBody(bw, child, depth);
         } else if (child instanceof J3ONPC) {
             endtag = writeJ3ONPC(bw, child, depth);
-        } else if (child instanceof CameraEntity) {
-            endtag = writeCameraEntity(bw, child, depth);
         } else if (child instanceof SituationEntity) {
             endtag = writeSituationEntity(bw, child, depth);
-        } else if (child instanceof SoundEntity) {
-            endtag = writeSoundEntity(bw, child, depth);
-        } else if (child instanceof PlayerStartEntity) {
-            endtag = writePlayerStartEntity(bw, child, depth);
-        } else if (child instanceof WayPointEntity) {
-            endtag = writeWaypointEntity(bw, child, depth);
-        } else if (child instanceof TriggerBox) {
-            endtag = writeTriggerBoxEntity(bw, child, depth);
-        } else if (child instanceof NPCLocationEntity) {
-            endtag = writeNPCLocationEntity(child, bw, depth);
-        } else if (child instanceof NavigationMesh) {
-            endtag = writeNavigationMesh(child, bw, depth);
-        }  else if (child instanceof PivotGizmo) {
+        } else if (child instanceof PivotGizmo) {
             endtag = writePivot(child, bw, depth);
         } else if (child instanceof Klatch) {
             endtag = writeKlatch((Klatch) child, bw, depth);
-        } else if (child instanceof Terrain) {
-            endtag = writeTerrain((Terrain) child, bw, depth);
         } else if (child instanceof BrushBatch) {
             endtag = writeBrushBatch((BrushBatch) child, bw, depth);
         } else if (child instanceof Prefab) {
@@ -510,7 +373,7 @@ public class SceneSaver {
      * @param depth
      * @return
      */
-    public static String defaultWritePrefab(BufferedWriter bw, Prefab child, int depth) throws IOException {
+    public static String defaultWritePrefab(Writer bw, Prefab child, int depth) throws IOException {
         ObjectType ot = child.getObjectType();
         XMLUtils.writeTabs(bw, depth);
         bw.write("<prefab ");
