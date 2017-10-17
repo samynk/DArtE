217fb5f8df6b57a2e63af527d7954024d27b0f49
samynk <samyn.koen@gmail.com>
2017-10-17 11:12:26 +0200

    Added rig loading and saving test methods.

--- test/dae/prefab/io/TextIOTestSuite.java
+++ test/dae/prefab/io/TextIOTestSuite.java
@@ -7,12 +7,15 @@ package dae.prefab.io;
 import com.jme3.asset.AssetKey;
 import com.jme3.asset.AssetManager;
 import dae.GlobalObjects;
+import dae.animation.rig.Rig;
+import dae.animation.rig.io.RigLoader;
+import dae.animation.rig.io.RigWriter;
 import dae.io.ObjectTypeReader;
 import dae.io.SceneLoader;
 import dae.io.SceneSaver;
+import dae.prefab.io.rig.ReadWriteRigTest;
 import dae.prefabs.Prefab;
 import dae.prefabs.brush.TerrainBrush;
-import dae.prefabs.lights.SpotLightPrefab;
 import dae.prefabs.types.ObjectType;
 import dae.prefabs.types.ObjectTypeCategory;
 import java.io.BufferedWriter;
@@ -24,31 +27,40 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.xml.sax.SAXException;
 
 /**
  *
  * @author Koen Samyn
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({dae.prefab.io.ReadWriteCopyStrokeTest.class,  
-    dae.prefab.io.ReadWriteCopyBrushTest.class, 
+@Suite.SuiteClasses({dae.prefab.io.ReadWriteCopyStrokeTest.class,
+    dae.prefab.io.ReadWriteCopyBrushTest.class,
     ReadWriteCopyMeshTest.class,
     ReadWriteCopyGridTest.class,
     ReadWriteCopyHingeJoint.class,
-    ReadWriteCopyWaypointTest.class, 
+    ReadWriteCopyWaypointTest.class,
     ReadWriteCopyCharacterPath.class,
     ReadWriteCopySpotLightTest.class,
-    ReadWriteCopyAmbientLightTest.class, 
-    ReadWriteCopyDirectionalLightTest.class, 
-    ReadWriteCopyPointLightTest.class})
+    ReadWriteCopyAmbientLightTest.class,
+    ReadWriteCopyDirectionalLightTest.class,
+    ReadWriteCopyPointLightTest.class,
+    ReadWriteCopySoundEntityTest.class,
+    ReadWriteCopyCameraEntityTest.class,
+    ReadWriteCopyPlayerStartEntityTest.class,
+    ReadWriteCopyTerrainTest.class,
+    ReadWriteCopyRevoluteJointTest.class
+})
 public class TextIOTestSuite {
-    public static void createMockGame(){
+
+    public static void createMockGame() {
         try {
             ObjectTypeReader reader = new ObjectTypeReader();
             MockAssetManager manager = new MockAssetManager();
@@ -60,21 +72,19 @@ public class TextIOTestSuite {
             Logger.getLogger(ReadWriteCopyStrokeTest.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public static Object createObject(String category, String label, Class expected)
-    {
+
+    public static Object createObject(String category, String label, Class expected) {
         ObjectTypeCategory types = GlobalObjects.getInstance().getObjectsTypeCategory();
         AssetManager manager = GlobalObjects.getInstance().getAssetManager();
         ObjectType type = types.getObjectType(category, label);
-        assertNotNull("Type ["+category+","+label+"] not found", type);
-        Object object = type.create(manager, label+"1");
-        assertNotNull("Could not create strokeObject", object);
-        
+        assertNotNull("Type [" + category + "," + label + "] not found", type);
+        Object object = type.create(manager, label + "1");
+        assertNotNull("Could not create " + category + ":" + label + "!", object);
         assertTrue("Stroke object not instanceof Stroke", expected.isInstance(object));
         return object;
     }
-    
-    public static String writeObject(Prefab toWrite){
+
+    public static String writeObject(Prefab toWrite) {
         dae.project.Level l = new dae.project.Level("test", false);
         l.attachChild(toWrite);
 
@@ -95,17 +105,47 @@ public class TextIOTestSuite {
     public static dae.project.Level readLevel(String result, Class expected, int childCount) {
         ObjectTypeCategory types = GlobalObjects.getInstance().getObjectsTypeCategory();
         AssetManager manager = GlobalObjects.getInstance().getAssetManager();
-        
+
         InputStream stream = new ByteArrayInputStream(result.getBytes(StandardCharsets.UTF_8));
         dae.project.Level loadedLevel = new dae.project.Level("test2", false);
         SceneLoader.loadScene(stream, manager, loadedLevel, types, null);
         List<TerrainBrush> childElements = loadedLevel.descendantMatches(expected);
-        assertEquals( childCount + " child element(s) expected", childCount, childElements.size());
+        assertEquals(childCount + " child element(s) expected", childCount, childElements.size());
         return loadedLevel;
     }
     
+    public static String writeRig(Rig rig){
+        StringWriter sw = new StringWriter();
+        BufferedWriter bw = new BufferedWriter(sw);
+        try {
+            
+            RigWriter.writeRig(bw, rig);
+            bw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(TextIOTestSuite.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        String result = sw.getBuffer().toString();
+        Logger.getLogger("DArtE").log(java.util.logging.Level.INFO, "Result of writing the object");
+        Logger.getLogger("DartE").log(java.util.logging.Level.INFO, result);
+        return result;
+    }
+    
+    public static Rig readRig(String contents){
+        ObjectTypeCategory types = GlobalObjects.getInstance().getObjectsTypeCategory();
+        AssetManager manager = GlobalObjects.getInstance().getAssetManager();
+
+        InputStream stream = new ByteArrayInputStream(contents.getBytes(StandardCharsets.UTF_8));
+        
+        try {
+            return RigLoader.readRig(stream,manager);
+        } catch (IOException | ParserConfigurationException | ClassNotFoundException | InstantiationException | SAXException | NumberFormatException | IllegalAccessException ex) {
+            Logger.getLogger(ReadWriteRigTest.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
     @Before
-    public void setup(){
+    public void setup() {
         createMockGame();
     }
 }
