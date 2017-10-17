b05a307eaa3978886281c77bbba7d998719ed6a4
samynk <samyn.koen@gmail.com>
2017-10-17 09:45:03 +0200

    removed comments.

--- src/dae/controller/ControllerLoader.java
+++ src/dae/controller/ControllerLoader.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package dae.controller;
 
 import com.jme3.asset.AssetInfo;
@@ -20,6 +16,5 @@ public class ControllerLoader implements AssetLoader{
         ParseFCL fclParser = new ParseFCL(assetInfo.openStream());
         FuzzySystem system = fclParser.getResult();
         return system;
-    }
-    
+    }  
 }
