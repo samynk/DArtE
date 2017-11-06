d6e868e8eb390c88d6db9aad46a2b1199ed37a25
samynk <samyn.koen@gmail.com>
2017-11-06 21:01:44 +0100

    Correct bug in dof2 calculations.

--- src/dae/util/MathUtil.java
+++ src/dae/util/MathUtil.java
@@ -2,17 +2,21 @@ package dae.util;
 
 import com.jme3.math.FastMath;
 import com.jme3.math.Quaternion;
+import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import dae.prefabs.AxisEnum;
 
 /**
  * This class offers a couple of useful functions , mostly related to difficult
  * rotation transformations.
+ *
  * @author Koen Samyn
  */
 public class MathUtil {
+
     /**
      * Creates a new reference frame starting from a single normal.
+     *
      * @param normal the normal to create a new rotation from.
      * @param mainAxis the axis of the object to align the normal with.
      * @return a new Quaternion.
@@ -50,9 +54,10 @@ public class MathUtil {
         }
         return result;
     }
-    
+
     /**
      * Creates a two degree freedom rotation from the provided normal.
+     *
      * @param from the start point of the rotation.
      * @param to the point to rotate the from point to over the two axis.
      * @param axis1 the first degree of freedom.
@@ -61,30 +66,34 @@ public class MathUtil {
      * will be created.
      * @return The quaternion with the DOF2 rotation.
      */
-    public static Quaternion createDof2Rotation(Vector3f from, Vector3f to, Vector3f axis1, Vector3f axis2, Quaternion q)
-    {
-        if ( q == null ){
+    public static Quaternion createDof2Rotation(Vector3f from, Vector3f to, Vector3f axis1, Vector3f axis2, Quaternion q, Vector2f angleOut) {
+        if (q == null) {
             q = new Quaternion();
         }
-        
-       float phi = calculateDof1Rotation(from, to, axis1);
-       float theta = calculateDof1Rotation(from,to, axis2);
+
+        float phi = calculateDof1Rotation(from, to, axis1);
+        q.fromAngleAxis(phi, axis1);
+        Vector3f p2 = q.mult(from);
+        Vector3f axis2r = q.mult(axis2);
+        float theta = calculateDof1Rotation(p2, to, axis2r);
         System.out.println("phi : " + FastMath.RAD_TO_DEG * phi);
         System.out.println("theta : " + FastMath.RAD_TO_DEG * theta);
-       
-        q.fromAngles(0,phi,theta);
+        angleOut.x = phi;
+        angleOut.y = theta;
+        q.fromAngles(0, phi, theta);
         return q;
     }
-    
+
     /**
-     * Calculates the angle of rotation to rotate the from point to the to point.
-     * @param from the point to rotate. 
+     * Calculates the angle of rotation to rotate the from point to the to
+     * point.
+     *
+     * @param from the point to rotate.
      * @param to the point to rotate to.
      * @param axis the axis over which to rotate.
      * @return the angle of the rotation.
      */
-    public static float calculateDof1Rotation(Vector3f from, Vector3f to, Vector3f axis)
-    {
+    public static float calculateDof1Rotation(Vector3f from, Vector3f to, Vector3f axis) {
         float dot1 = axis.dot(from);
         Vector3f pFrom = from.subtract(axis.mult(dot1));
         pFrom.normalizeLocal();
@@ -92,9 +101,23 @@ public class MathUtil {
         Vector3f pTo = to.subtract(axis.mult(dot2));
         pTo.normalizeLocal();
         float angle = FastMath.acos(pFrom.dot(pTo));
-        
+
         Vector3f dir = pFrom.cross(pTo);
-        angle = dir.dot(axis)>0?angle:-angle;
+        angle = dir.dot(axis) > 0 ? angle : -angle;
         return angle;
+    }
+
+    /**
+     * Projects a vector onto the plane going through point [0,0,0] and with a
+     * normal defined by the normal parameter.
+     *
+     * @param toProject the point to project.
+     * @param normal the normal of the plane.
+     */
+    public static void project(Vector3f toProject, Vector3f normal) {
+        float dot = normal.dot(toProject);
+        toProject.x = toProject.x - dot * normal.x;
+        toProject.y = toProject.y - dot * normal.y;
+        toProject.z = toProject.z - dot * normal.z;
     }
 }
