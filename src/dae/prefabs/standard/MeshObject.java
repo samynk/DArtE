19ec25fc9335adbb4121a327d65131cc23a31907
samynk <samyn.koen@gmail.com>
2017-11-08 10:17:50 +0100

    Removed unnecessary imports.

--- src/dae/prefabs/standard/MeshObject.java
+++ src/dae/prefabs/standard/MeshObject.java
@@ -2,18 +2,13 @@ package dae.prefabs.standard;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.scene.Node;
-import com.jme3.scene.Spatial;
 import dae.animation.skeleton.BodyElement;
 import dae.components.MeshComponent;
-import dae.io.XMLUtils;
 import dae.prefabs.Prefab;
-import dae.prefabs.gizmos.Gizmo;
-import java.io.IOException;
-import java.io.Writer;
 
 /**
  *
- * @author Koen
+ * @author Koen Samyn
  */
 public class MeshObject extends Prefab implements BodyElement {
 
@@ -36,41 +31,6 @@ public class MeshObject extends Prefab implements BodyElement {
         return (Node) clone();
     }
 
-    @Override
-    public Spatial clone() {
-        MeshObject mo = new MeshObject();
-        mo.setName(this.getName());
-        mo.setType(this.getType());
-        mo.setCategory(this.getCategory());
-        mo.setLocalScale(this.getLocalScale().clone());
-        mo.setLocalPrefabRotation(this.getLocalPrefabRotation().clone());
-        mo.setLocalPrefabTranslation(this.getLocalPrefabTranslation().clone());
-        mo.setPhysicsMesh(this.getPhysicsMesh());
-
-        for (Spatial s : this.children) {
-            if (!(s instanceof Gizmo)) {
-                mo.attachChild(s.clone(true));
-            }
-        }
-
-        return mo;
-    }
-
-    @Override
-    public Prefab duplicate(AssetManager manager) {
-//        MeshObject mo = new MeshObject();
-//        mo.setType(this.getType());
-//        mo.setCategory(this.getCategory());
-//        mo.create(this.meshName, manager, meshFile);
-//
-//        mo.setLocalScale(this.getLocalScale().clone());
-//        mo.setLocalRotation(this.getLocalRotation().clone());
-//        mo.setLocalTranslation(this.getLocalTranslation().clone());
-//        mo.setPhysicsMesh(this.getPhysicsMesh());
-//
-//        return mo;
-        return (Prefab) clone();
-    }
 
     public String getMeshName() {
         return meshName;
@@ -87,57 +47,14 @@ public class MeshObject extends Prefab implements BodyElement {
         }
     }
 
+    @Override
     public void attachBodyElement(BodyElement element) {
         if (element instanceof Node) {
             this.attachChild((Node) element);
         }
     }
 
+    @Override
     public void reset() {
-    }
-
-    public void write(Writer w, int depth) throws IOException {
-        for (int i = 0; i < depth; ++i) {
-            w.write('\t');
-        }
-        w.write("<prefab ");
-        XMLUtils.writeAttribute(w, "class", this.getClass().getName());
-        XMLUtils.writeAttribute(w, "name", this.getName());
-        XMLUtils.writeAttribute(w, "category", this.getCategory());
-        XMLUtils.writeAttribute(w, "type", this.getType());
-        XMLUtils.writeAttribute(w, "prefix", this.getPrefix());
-        XMLUtils.writeAttribute(w, "offset", this.getOffset());
-        XMLUtils.writeAttribute(w, "translation", this.getLocalTranslation());
-        XMLUtils.writeAttribute(w, "rotation", this.getLocalRotation());
-        XMLUtils.writeAttribute(w, "scale", this.getLocalScale());
-        if (hasPhysicsMesh()) {
-            XMLUtils.writeAttribute(w, "physicsMesh", getPhysicsMesh());
-        }
-        XMLUtils.writeAttribute(w, "shadowmode", getShadowMode().toString());
-
-
-
-        boolean hasBodyElements = false;
-        for (Spatial child : this.getChildren()) {
-            if (child instanceof BodyElement) {
-                hasBodyElements = true;
-                break;
-            }
-        }
-
-        if (!hasBodyElements) {
-            w.write("/>\n");
-        } else {
-            w.write(">\n");
-            for (Spatial child : this.getChildren()) {
-                if (child instanceof BodyElement) {
-                    ((BodyElement) child).write(w, depth + 1);
-                }
-            }
-            for (int i = 0; i < depth; ++i) {
-                w.write('\t');
-            }
-            w.write("</prefab>\n");
-        }
     }
 }
