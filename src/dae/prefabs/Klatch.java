1a861b23677e353eef26b06fb0c19f4185c55d4e
samynk <samyn.koen@gmail.com>
2017-11-20 12:23:50 +0100

    Removed unnecessary comments. Removed duplicate method.

--- src/dae/prefabs/Klatch.java
+++ src/dae/prefabs/Klatch.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package dae.prefabs;
 
 import com.jme3.asset.AssetManager;
@@ -58,12 +54,14 @@ public class Klatch extends Prefab {
         return k;
     }
 
+    /*
     @Override
-    public Prefab duplicate(AssetManager assetManager) {
+    public Prefab duplicate(AssetManager assetManager, boolean duplicateChildren) {
         Prefab duplicate = (Prefab)clone();
         duplicate.setLocalPrefabTranslation(this.getWorldTranslation());
-        return (Prefab)clone();
+        return (Prefab)super.duplicate(assetManager,duplicateChildren);
     }
+    */
     
     
     
