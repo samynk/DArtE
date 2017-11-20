567b2f6d6dab2ecdfc77f2bc6c17f6908f99b483
samynk <samyn.koen@gmail.com>
2017-11-20 12:10:26 +0100

    Removed unnecessary comments.

--- src/dae/animation/rig/gui/RigConnectorDialog.java
+++ src/dae/animation/rig/gui/RigConnectorDialog.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package dae.animation.rig.gui;
 
 import dae.animation.rig.AnimationListControl;
