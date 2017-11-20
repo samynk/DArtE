90dd0c55825d62428e20deb92b54d7d6ad3d4a54
samynk <samyn.koen@gmail.com>
2017-11-20 13:14:04 +0100

    new test for the revolute two axis joint.

---
+++ test/dae/prefab/io/ReadWriteCopyRevoluteJointTwoAxisTest.java
@@ -0,0 +1,83 @@
+package dae.prefab.io;
+
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import dae.GlobalObjects;
+import dae.animation.skeleton.RevoluteJoint;
+import dae.animation.skeleton.RevoluteJointTwoAxis;
+import dae.components.ObjectComponent;
+import dae.components.PrefabComponent;
+import dae.prefabs.Prefab;
+import dae.project.Level;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Koen.Samyn
+ */
+public class ReadWriteCopyRevoluteJointTwoAxisTest {
+
+    public ReadWriteCopyRevoluteJointTwoAxisTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        if (GlobalObjects.getInstance().getAssetManager() == null) {
+            TextIOTestSuite.createMockGame();
+        }
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void readWriteRevoluteJoint() {
+        RevoluteJointTwoAxis rj = (RevoluteJointTwoAxis) TextIOTestSuite.createObject("Animation", "RevoluteJointTwoAxis", RevoluteJointTwoAxis.class);
+        ObjectComponent oc = (ObjectComponent) rj.getComponent("ObjectComponent");
+        assertNotNull(oc);
+        PrefabComponent tc = rj.getComponent("TransformComponent");
+        assertNotNull(tc);
+        assertEquals("RevoluteJointTwoAxis1", oc.getName());
+        assertEquals("default", oc.getLayer());
+
+        //rj.setChainChildName("revolutejoint2");
+
+        String result = TextIOTestSuite.writeObject(rj);
+
+        Level loadedLevel = TextIOTestSuite.readLevel(result, RevoluteJointTwoAxis.class, 1);
+        RevoluteJointTwoAxis loaded = loadedLevel.descendantMatches(RevoluteJointTwoAxis.class).get(0);
+
+        assertEquals(loaded.getName(), rj.getName());
+        assertEquals(loaded.getGroup(), rj.getGroup());
+        assertEquals(loaded.getRadius(), rj.getRadius(), 0.0001f);
+        assertEquals(loaded.getHeight(), rj.getHeight(), 0.0001f);
+        
+        // there should be only one child.
+        assertEquals(loaded.getChildren().size() , 1);
+        Spatial transformNode = loaded.getChild(0);
+        assertTrue(transformNode instanceof Node);
+        Node node = (Node)transformNode;
+        
+        
+        List<Prefab> childPrefabs = node.descendantMatches(Prefab.class);
+        assertEquals(childPrefabs.size(),1);
+    }
+}
