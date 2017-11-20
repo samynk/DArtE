84ece39f3e745b5a1b1c8502b3de6e4f8f013284
samynk <samyn.koen@gmail.com>
2017-11-20 12:32:31 +0100

    Removed unnecessary comments.

--- src/dae/prefabs/ui/events/AssetEventType.java
+++ src/dae/prefabs/ui/events/AssetEventType.java
@@ -1,12 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package dae.prefabs.ui.events;
 
 /**
  *
- * @author samyn_000
+ * @author Koen Samyn
  */
 public enum AssetEventType {
     EDIT,DELETE,RENAME,MODIFIED;
