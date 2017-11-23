c24bb5efaea2b08e04bdef491cdddcf4a26da807
samynk <samyn.koen@gmail.com>
2017-11-23 09:01:40 +0100

    Removed unnecessary comments.

--- src/dae/animation/skeleton/RJLogSymbol.java
+++ src/dae/animation/skeleton/RJLogSymbol.java
@@ -1,12 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package dae.animation.skeleton;
 
 /**
  *
- * @author Koen
+ * @author Koen Samyn
  */
 public enum RJLogSymbol {
 
