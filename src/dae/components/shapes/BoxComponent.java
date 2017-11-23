9f04ac12520d36f0eec2466b8d12efb7e4a9e8e7
samynk <samyn.koen@gmail.com>
2017-11-23 09:05:52 +0100

    Added color and visibility properties.

--- src/dae/components/shapes/BoxComponent.java
+++ src/dae/components/shapes/BoxComponent.java
@@ -4,12 +4,13 @@
 package dae.components.shapes;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.material.MatParam;
 import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
-import com.jme3.scene.shape.Sphere;
 import dae.GlobalObjects;
 import dae.components.PrefabComponent;
 import dae.prefabs.Prefab;
@@ -20,52 +21,61 @@ import dae.prefabs.Prefab;
  */
 public class BoxComponent extends PrefabComponent {
 
-   
     private float length;
     private float width;
     private float height;
-    
+
     private Vector3f center;
-    
+
+    private boolean visible;
+    private ColorRGBA diffuseColor;
+    private Material boxMat;
+
     private Geometry boxGeo;
     private Prefab parent;
 
     @Override
     public void install(Prefab parent) {
         this.parent = parent;
-        createShape(parent);
-        
+        recreate();
     }
 
     private void createShape(Prefab parent) {
-        Vector3f min = center.subtract(length/2, width/2, height/2);
-        Vector3f max = center.add(length/2, width/2, height/2);
-        Box box = new Box(min,max);
+        Vector3f min = center.subtract(length / 2, width / 2, height / 2);
+        Vector3f max = center.add(length / 2, width / 2, height / 2);
+        Box box = new Box(min, max);
         boxGeo = new Geometry("box", box);
-        
-        AssetManager am = GlobalObjects.getInstance().getAssetManager();
-        Material boneMat = am.loadMaterial("Materials/RigMaterial.j3m");
 
-        boxGeo.setMaterial(boneMat);
-        
+        AssetManager am = GlobalObjects.getInstance().getAssetManager();
+        boxMat = am.loadMaterial("Materials/RigMaterial.j3m");
+        boxGeo.setMaterial(boxMat);
+        resetColor();
+
         parent.attachChild(boxGeo);
     }
-    
-    public void recreate(){
-       deinstall();
-       createShape(parent);
+
+    private void resetColor() {
+        MatParam color = boxMat.getParam("Color");
+        if (color != null) {
+            color.setValue(diffuseColor);
+        }
+    }
+
+    public void recreate() {
+        deinstall();
+        createShape(parent);
     }
 
     @Override
     public void deinstall() {
-        if ( boxGeo != null){
+        if (boxGeo != null) {
             boxGeo.removeFromParent();
         }
     }
 
     @Override
     public void installGameComponent(Spatial parent) {
-        
+
     }
 
     /**
@@ -112,8 +122,8 @@ public class BoxComponent extends PrefabComponent {
         this.height = height;
         recreate();
     }
-    
-     /**
+
+    /**
      * @return the center
      */
     public Vector3f getCenter() {
@@ -126,7 +136,32 @@ public class BoxComponent extends PrefabComponent {
     public void setCenter(Vector3f center) {
         this.center = center;
         recreate();
-        
+
     }
-    
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+        if (boxGeo != null) {
+            if (!visible) {
+                boxGeo.removeFromParent();
+            } else {
+                if (boxGeo.getParent() == null) {
+                    parent.attachChild(boxGeo);
+                }
+            }
+        }
+    }
+
+    public boolean getVisible() {
+        return visible;
+    }
+
+    public ColorRGBA getDiffuseColor() {
+        return diffuseColor;
+    }
+
+    public void setDiffuseColor(ColorRGBA diffuseColor) {
+        this.diffuseColor = diffuseColor;
+        resetColor();
+    }
 }
