e44c0c8b3d131ddb2df8c498d94962808c10c3a1
samynk <samyn.koen@gmail.com>
2017-11-28 07:49:21 +0100

    Removed //todo comment.

--- test/dae/prefab/standard/DictionaryParameterTest.java
+++ test/dae/prefab/standard/DictionaryParameterTest.java
@@ -56,11 +56,6 @@ public class DictionaryParameterTest {
     @After
     public void tearDown() {
     }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 
     @Test
     public void testAddRemoveKeys() {
