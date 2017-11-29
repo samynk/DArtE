6e58320cdd106793e772d959432ee70b21337fa1
samynk <samyn.koen@gmail.com>
2017-11-29 18:07:53 +0100

    General interface for tools in the timeline view.

---
+++ src/dae/animation/rig/timing/gui/TimingTool.java
@@ -0,0 +1,20 @@
+/*
+ * Digital Arts and Entertainment 
+ */
+package dae.animation.rig.timing.gui;
+
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author Koen.Samyn
+ */
+public interface TimingTool {
+
+    public void mouseClicked(MouseEvent e);
+
+    public void mousePressed(MouseEvent e);
+
+    public void mouseReleased(MouseEvent e);
+
+}
