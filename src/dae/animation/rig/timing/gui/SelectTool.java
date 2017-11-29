cb96e1f08ca127ee6ea2b7d94611b1ea3ad0d3f4
samynk <samyn.koen@gmail.com>
2017-11-29 18:07:02 +0100

    SelectTool to select keys from the time line.

---
+++ src/dae/animation/rig/timing/gui/SelectTool.java
@@ -0,0 +1,38 @@
+/*
+ * Digital Arts and Entertainment 
+ */
+package dae.animation.rig.timing.gui;
+
+import dae.animation.rig.timing.Behaviour;
+import java.awt.event.MouseEvent;
+
+/**
+ *
+ * @author Koen.Samyn
+ */
+public class SelectTool implements TimingTool{
+    private final FramePanel parent;
+    
+    public SelectTool(FramePanel parent){
+        this.parent = parent;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {        
+        Behaviour current = parent.getBehaviour();
+        if ( current != null ){
+            current.setCurrentFrame(parent.mouseXToFrame(e.getX()));
+            parent.repaint();
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+}
